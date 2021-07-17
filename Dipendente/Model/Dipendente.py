dac0319bea815e9a288e6fb8885877162f81f1fe
«Marco Olivi» <S1092635@studenti.univpm.it>
2021-07-17 16:16:54 +0200

    model dipendente

---
+++ Dipendente/Model/Dipendente.py
@@ -0,0 +1,9 @@
+
+class Dipendente():
+    def __init__(self, id, nome, cognome, email, cellulare):
+        super(Dipendente, self).__init__()
+        self.id = id
+        self.nome = nome
+        self.cognome = cognome
+        self.email = email
+        self.cellulare = cellulare
