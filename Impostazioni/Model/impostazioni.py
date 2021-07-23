2db16b907d1089df934aa9b007215f93ef91b35f
Davide <S1094698@studenti.univpm.it>
2021-07-23 23:13:45 +0200

    tolto tutte le bestemmie sistemato nomi interfaccia

---
+++ Impostazioni/Model/impostazioni.py
@@ -0,0 +1,9 @@
+class Impostazioni():
+    def __init__(self, orarioApertura, orarioChiusura, numeroSale, tariffaSala, tariffaIncisione, tariffaMix):
+        super(Impostazioni, self).__init__()
+        self.orarioApertura = orarioApertura
+        self.orarioChiusura = orarioChiusura
+        self.numeroSale = numeroSale
+        self.tariffaSala = tariffaSala
+        self.tariffaIncisione = tariffaIncisione
+        self.tariffaMix = tariffaMix
